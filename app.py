6876eefa90471632172d2264dc68f27b1b430227
23127406-23127423 <ngtanloc759kt1@gmail.con>
2026-01-07 11:50:21 +0700

    add new code

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     return "NTL{H3llo_Pr0j3ct_CI/CD w1th j3nk1ns, D0ck3er!!}"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)

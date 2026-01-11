ab763b7a3a53d6bdc246c15f693e719f612e6532
NguyenTanLoc1204 <ngtanloc759kt1@gmail.con>
2026-01-11 08:16:17 +0700

    new code

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def search():
         return f"Sai password: {user_input}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3001, debug=True)
